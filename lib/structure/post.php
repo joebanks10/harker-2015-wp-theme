cd2ff030955bf020de9bc411d9fcbea28dab37ba
Joe Banks <joeb@harker.org>
2016-07-29 14:41:12 -0700

    Return post info if post info option is empty

--- lib/structure/post.php
+++ lib/structure/post.php
@@ -4,7 +4,11 @@ add_filter( 'genesis_post_info', 'hkr_archive_post_info' );
 
 function hkr_archive_post_info($post_info) {
     if ( ! is_single() ) {
-        $post_info = genesis_get_option('hkr_archive_post_info');
+        $option = genesis_get_option('hkr_archive_post_info');
+        
+        if (!empty($option)) {
+            $post_info = $option;
+        }
     }
 
     return $post_info;
